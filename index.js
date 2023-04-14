c713d1f5447a229692f9c2e32ee62cda2a0f077a
= <=>
2023-04-13 23:55:27 -0700

    javascript-objects

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {
+    name: "Tobase",
+    streetAddress: "123 Main Street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+
+    const newEmployee = { ...employee };
+
+    newEmployee[key] = value;
+    return newEmployee;
+
+};
+ employee = updateEmployeeWithKeyAndValue
+
+
+
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  }
+
+  const newEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Tob");
+
+  newEmployee;
+
+  function deleteFromEmployeeByKey(object, key) {
+    var newObj = Object.assign({}, object);
+    delete newObj[key];
+    return newObj;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
+    return object;
+  }
